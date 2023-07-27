494c9bb1c2507ab3cc82d2f3306584099e5b132a
sintiadc <122029255+sintiadc@users.noreply.github.com>
2023-07-27 14:30:56 +0700

    Update sortCarByYearDecendingly.js

--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -8,9 +8,11 @@ function sortCarByYearDescendingly(cars) {
 
   // Tulis code-mu disini
   for (let i = 0; i < result.length-1; i++) {
-    for (let j = 0; j < result.length-1; j++) {
-      if (result[j].year < result[j + 1].year) {
-        [result[j], result[j + 1]] = [result[j + 1], result[j]];
+    for (let j = 0; j < result.length-i-1; j++) {
+      if (result[j + 1].year > result[j].year) {
+        const temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
       }
     }
   }
